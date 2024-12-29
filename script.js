d0df530373ad0c154033eacfb10ce3dfe441b957
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-12-29 18:30:57 -0300

    atualizando taxa de câmbio

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function converter() {
     }
 
     // Taxas de câmbio (essas taxas variam de acordo com a cotação)
-    const taxaDolar = 5.74;
-    const taxaEuro = 6.14;
+    const taxaDolar = 6.19;
+    const taxaEuro = 6.46;
 
     // Calcula o valor em dólares e euros
     let dolar = real / taxaDolar;
